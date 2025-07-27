92243100f98b88dd9bb0516e9050a20c4e01fc05
jin1963 <kaojino89@gmail.com>
2025-07-27 12:01:18 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const contractAddress = "0xBaeF58FC0Eb20334b2fDEC4882e2AB972C1242DE"; // Smart Contract ที่ใช้จริง
+const usdtAddress = "0x55d398326f99059fF775485246999027B3197955"; // USDT on BNB Chain
+const targetChainId = 56; // BNB Chain ID
